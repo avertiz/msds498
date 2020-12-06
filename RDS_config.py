4cd13b6b57b3873df43844480abc01da77d13d8d
avertiz <63977920+avertiz@users.noreply.github.com>
2020-12-05 22:17:44 -0600

    removing RDS instance info
    removing RDS instance info

--- RDS_config.py
+++ RDS_config.py
@@ -1,5 +1,5 @@
-host = 'msds498.csf5xux23pe2.us-east-2.rds.amazonaws.com'
-port = 5432
-user = 'avertiz'
-password = 'MSDS498_vertiz!'
-dbname = 'msds498'
+host = []
+port = []
+user = []
+password = []
+dbname = []
